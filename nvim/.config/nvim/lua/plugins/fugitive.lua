ec5403796de4fa607688efd28fbc87c83e8fe9a8
glaspal6 <dylan.kamstra@protonmail.com>
2024-01-29 12:57:24 +0200

    Added some packages/changed structure

---
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,28 @@
+return {
+    "tpope/vim-fugitive",
+    config = function()
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+
+        local autocmd = vim.api.nvim_create_autocmd
+        autocmd("BufWinEnter", {
+            group = dyl_Fugitive,
+            pattern = "*",
+            callback = function()
+                if vim.bo.ft ~= "fugitive" then
+                    return
+                end
+
+                local bufnr = vim.api.nvim_get_current_buf()
+                local opts = {buffer = bufnr, remap = false}
+                vim.keymap.set("n", "<leader>p", function()
+                    vim.cmd.Git('push')
+                end, opts)
+
+                -- rebase always
+                vim.keymap.set("n", "<leader>P", function()
+                    vim.cmd.Git('pull')
+                end, opts)
+            end,
+        })
+    end
+}
