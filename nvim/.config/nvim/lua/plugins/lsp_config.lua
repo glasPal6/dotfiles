ffcb86532400a5467b636f3becf315d976927343
glaspal6 <dylan.kamstra@protonmail.com>
2024-01-29 19:56:54 +0200

    Changed the file browser

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -1,54 +1,65 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", "cmake" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require('lspconfig')
-            lspconfig.lua_ls.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.cmake.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "cmake" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.cmake.setup({})
 
-            vim.api.nvim_create_autocmd('LspAttach', {
-                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-                callback = function(ev)
-                    -- Enable completion triggered by <c-x><c-o>
-                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-                    -- Buffer local mappings.
-                    -- See `:help vim.lsp.*` for documentation on any of the below functions
-                    local opts = { buffer = ev.buf }
-                    -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                    -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                    -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                    -- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-                    -- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                    -- vim.keymap.set('n', '<leader>wl', function()
-                    -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                    -- end, opts)
-                    -- vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-                    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-                    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                    -- vim.keymap.set('n', '<leader>f', function()
-                    --     vim.lsp.buf.format { async = true }
-                    -- end, opts)
-                end,
-            })
-        end
-    }
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gd", function()
+						vim.lsp.buf.definition()
+					end, opts)
+					vim.keymap.set("n", "K", function()
+						vim.lsp.buf.hover()
+					end, opts)
+					vim.keymap.set("n", "<leader>vws", function()
+						vim.lsp.buf.workspace_symbol()
+					end, opts)
+					vim.keymap.set("n", "<leader>vd", function()
+						vim.diagnostc.open_float()
+					end, opts)
+					vim.keymap.set("n", "[d", function()
+						vim.diagnostic.goto_next()
+					end, opts)
+					vim.keymap.set("n", "]d", function()
+						vim.diagnostic.goto_prev()
+					end, opts)
+					vim.keymap.set("n", "<leader>ca", function()
+						vim.lsp.buf.code_action()
+					end, opts)
+					vim.keymap.set("n", "<leader>rr", function()
+						vim.lsp.buf.references()
+					end, opts)
+					vim.keymap.set("n", "<leader>rn", function()
+						vim.lsp.buf.rename()
+					end, opts)
+					vim.keymap.set("i", "<C-h>", function()
+						vim.lsp.buf.signature_help()
+					end, opts)
+				end,
+			})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,30 +1,49 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
-        dependencies = {
-            'nvim-lua/plenary.nvim'
-        },
-        config = function()
-            local builtin = require("telescope.builtin")
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-            -- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        end
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup {
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {
-                        }
-                    }
-                }
-            }
-            require("telescope").load_extension("ui-select")
-        end
-    }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+			-- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			vim.keymap.set("n", "<space>pm", ":Telescope file_browser<CR>")
+			require("telescope").setup({
+				extensions = {
+					file_browser = {
+						-- theme = "ivy",
+						hijack_netrw = true,
+					},
+				},
+			})
+			require("telescope").load_extension("file_browser")
+		end,
+	},
 }
