a2a34213f745135721df3dc43a976d0f66945d35
glaspal6 <dylan.kamstra@protonmail.com>
2024-01-29 11:23:08 +0200

    Changed the config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,59 @@
-require("dyl")
+vim.cmd("set expandtab")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set tabstop=4")
+
+-- Lazy NVim Package Manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+local plugins = {
+    {   
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    }
+}
+
+-- Lazy options
+local opts = {}
+
+-- Load Plugins
+vim.g.mapleader = ' '
+
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+    ensure_installed = {"lua", "c", "cpp", "python"},
+    highlight = { enable = true },
+    indent = { enable = true }, 
+})
+
+vim.cmd[[colorscheme tokyonight]]
