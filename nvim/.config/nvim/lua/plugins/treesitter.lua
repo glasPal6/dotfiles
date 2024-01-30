164f6853c817e578144869445b49eb08e11cd0d4
glaspal6 <dylan.kamstra@protonmail.com>
2024-01-30 11:03:44 +0200

    Added dap for C, but removed it

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local config = require("nvim-treesitter.configs")
         config.setup({
             auto_install = true,
-            ensure_installed = {"lua", "vimdoc", "c", "cpp", "python"},
+            ensure_installed = {"lua", "vimdoc", "c", "cpp"},
             highlight = { enable = true },
             indent = { enable = true },
         })
