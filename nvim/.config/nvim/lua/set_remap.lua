2e18f445efb6234be39e73c1102df3d5b1f9274b
glaspal6 <dylan.r.kamstra@gmail.com>
2024-04-27 10:41:14 +0200

    Removed the history search for /

--- nvim/.config/nvim/lua/set_remap.lua
+++ nvim/.config/nvim/lua/set_remap.lua
@@ -38,18 +38,14 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", ":", "q:i")
-vim.keymap.set("n", "/", "q/i")
-vim.keymap.set("n", "?", "q?i")
+-- vim.keymap.set("n", "/", "q/i")
+-- vim.keymap.set("n", "?", "q?i")
 
 vim.keymap.set("n", "<leader>nv", ":vsplit<CR>:Telescope file_browser<CR>")
 vim.keymap.set("n", "<leader>ns", ":split<CR>:Telescope file_browser<CR>")
