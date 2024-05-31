b4c08ae9a9d19dc93671d77a79e1681a5634675f
glaspal6 <dylan.r.kamstra@gmail.com>
2024-05-31 16:25:12 +0200

    feat(nvim): Updated nvim

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -1,6 +1,21 @@
 return {
-    "mbbill/undotree",
-    config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-    end
+	"jiaoshijie/undotree",
+	dependencies = "nvim-lua/plenary.nvim",
+	keys = { -- load the plugin only when using it's keybinding:
+		{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+	},
+	config = function()
+		require("undotree").setup({
+            float_diff = false,
+			keymaps = {
+				["<Down>"] = "move_next",
+				["<Up>"] = "move_prev",
+				["g<Down>"] = "move2parent",
+				["<C-Down>"] = "move_change_next",
+				["<C-Up>"] = "move_change_prev",
+				["<cr>"] = "action_enter",
+				["p"] = "enter_diffbuf",
+			},
+		})
+	end,
 }
