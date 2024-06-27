b98b6939a616a7f116902e291f07086c1fb357a1
glaspal6 <dylan.r.kamstra@gmail.com>
2024-06-27 07:44:28 +0200

    feat(awesome): Refactored and edited

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -21,7 +21,7 @@ local debian = require("debian.menu")
 local has_fdo, freedesktop = pcall(require, "freedesktop")
 
 require("configuration")
-terminal = require("configuration.apps").default.terminal
+local terminal = require("configuration.apps").default.terminal
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
