b6afaf114c251dae728a14a83a69c1749e2c2670
glaspal6 <dylan.r.kamstra@gmail.com>
2024-09-27 10:38:10 +0200

    feat(nvim): update

--- nvim/.config/nvim/lua/plugins/ai_assistants.lua
+++ nvim/.config/nvim/lua/plugins/ai_assistants.lua
@@ -13,37 +13,11 @@ return {
             vim.keymap.set("i", "<A-r>", function() neocodeium.cycle_or_complete(-1) end)
         end,
     }
-	-- {
-	-- 	"Exafunction/codeium.vim",
-	-- 	config = function()
-    --         vim.g.codeium_enabled = false
-    --         vim.g.codeium_disable_bindings = true
-
-	-- 		-- Change '<C-g>' here to any keycode you like.
-	-- 		vim.keymap.set("i", "<C-y>", function()
-	-- 			return vim.fn["codeium#Accept"]()
-	-- 		end, { expr = true, silent = true })
-
-	-- 		-- vim.keymap.set("i", "<A-n>", function()
-	-- 		-- 	return vim.fn["codeium#CycleCompletions"](1)
-	-- 		-- end, { expr = true, silent = true })
-
-	-- 		-- vim.keymap.set("i", "<A-p>", function()
-	-- 		-- 	return vim.fn["codeium#CycleCompletions"](-1)
-	-- 		-- end, { expr = true, silent = true })
-
-	-- 		-- vim.keymap.set("i", "<A-x>", function()
-	-- 		-- 	return vim.fn["codeium#Clear"]()
-	-- 		-- end, { expr = true, silent = true })
-
-	-- 		vim.keymap.set("n", "<AS-c>", vim.cmd.CodeiumToggle, { expr = true, silent = true })
-	-- 	end,
-	-- },
-	-- {
-	-- 	"github/copilot.vim",
-	-- 	config = function()
+    -- {
+    -- 	"github/copilot.vim",
+    -- 	config = function()
     --         vim.g.copilot_enabled = false
-	-- 		vim.keymap.set("n", "<leader>cp", vim.cmd.Copilot, { silent = true })
-	-- 	end,
-	-- },
+    -- 		vim.keymap.set("n", "<leader>cp", vim.cmd.Copilot, { silent = true })
+    -- 	end,
+    -- },
 }
--- nvim/.config/nvim/lua/plugins/git_plugins.lua
+++ nvim/.config/nvim/lua/plugins/git_plugins.lua
@@ -1,35 +1,4 @@
 return {
-    -- {
-    --     "tpope/vim-fugitive",
-    --     config = function()
-    --         vim.keymap.set("n", "<leader>gs", ":vert Git<CR><C-w>o");
-    --         vim.keymap.set("n", "<leader>gb", ":Git blame<CR>");
-    --         vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>");
-    --         vim.keymap.set("n", "<leader>gt", ":vert Git log --graph --decorate --abbrev-commit --oneline <CR>");
-    --         vim.keymap.set("n", "<leader>gl", ":vsplit<CR>:0Gllog<CR>");
-
-    --         local autocmd = vim.api.nvim_create_autocmd
-    --         autocmd("BufWinEnter", {
-    --             pattern = "*",
-    --             callback = function()
-    --                 if vim.bo.ft ~= "fugitive" then
-    --                     return
-    --                 end
-
-    --                 local bufnr = vim.api.nvim_get_current_buf()
-    --                 local opts = {buffer = bufnr, remap = false}
-
-    --                 vim.keymap.set("n", "<leader>P", function()
-    --                     vim.cmd.Git('push')
-    --                 end, opts)
-
-    --                 vim.keymap.set("n", "<leader>p", function()
-    --                     vim.cmd.Git('pull')
-    --                 end, opts)
-    --             end,
-    --         })
-    --     end
-    -- },
     {
         "NeogitOrg/neogit",
         dependencies = {
