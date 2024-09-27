e1157e574e6a8dde3e6344cb4427d1132cc322ca
glaspal6 <dylan.r.kamstra@gmail.com>
2024-09-27 10:23:27 +0200

    feat(nvim): nvim update

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,65 +1,77 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			local builtin = require("telescope.builtin")
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            local builtin = require("telescope.builtin")
             -- vim.keymap.set("n", "<leader>pm", ":Telescope find_files hidden=true<CR>", {})
             vim.keymap.set("n", "<leader>pm", ":Telescope find_files<CR>", {})
-			vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>ps", builtin.grep_string, {})
-			vim.keymap.set("n", "<leader>pt", builtin.treesitter, {})
+            vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>ps", builtin.grep_string, {})
+            vim.keymap.set("n", "<leader>pt", builtin.treesitter, {})
             -- vim.keymap.set("n", "<leader>M", builtin.marks, {})
-			require("telescope").setup({
-				defaults = {
-					layout_config = {
-						prompt_position = "top",
-					},
-					prompt_prefix = " ",
-					sorting_strategy = "ascending",
-					dynamic_preview_title = true,
+            require("telescope").setup({
+                defaults = {
+                    layout_config = {
+                        prompt_position = "top",
+                    },
+                    prompt_prefix = " ",
+                    sorting_strategy = "ascending",
+                    dynamic_preview_title = true,
                     color_devicons = true,
-				},
-			})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
-	-- {
-	-- 	"nvim-telescope/telescope-file-browser.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-telescope/telescope.nvim",
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"nvim-tree/nvim-web-devicons",
-	-- 	},
-	-- 	config = function()
-	-- 		vim.keymap.set("n", "<leader>pm", ":Telescope file_browser<CR>")
-	-- 		require("telescope").setup({
-	-- 			extensions = {
-	-- 				file_browser = {
-	-- 					-- theme = "ivy",
-	-- 					hijack_netrw = true,
-	-- 					initial_browser = "tree",
-	-- 					auto_depth = true,
-	-- 					depth = 1,
-	-- 				},
-	-- 			},
-	-- 		})
-	-- 		require("telescope").load_extension("file_browser")
-	-- 	end,
-	-- },
+                },
+            })
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            require("telescope").load_extension("ui-select")
+        end,
+    },
+    {
+        "debugloop/telescope-undo.nvim",
+        dependencies = {
+            {
+                "nvim-telescope/telescope.nvim",
+                dependencies = { "nvim-lua/plenary.nvim" },
+            },
+        },
+        keys = {
+            { -- lazy style key map
+                "<leader>u",
+                "<cmd>Telescope undo<cr>",
+                desc = "undo history",
+            },
+        },
+        opts = {
+            extensions = {
+                undo = {
+                    use_delta = true,
+                    use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
+                    side_by_side = true,
+                    vim_diff_opts = {
+                        ctxlen = vim.o.scrolloff,
+                    },
+                    entry_format = "#$ID, $STAT, $TIME",
+                    time_format = "",
+                    saved_only = false,
+                },
+            },
+        },
+        config = function(_, opts)
+            require("telescope").setup(opts)
+            require("telescope").load_extension("undo")
+        end,
+    },
 }
--- nvim/.config/nvim/lua/plugins/vim_mux.lua
+++ nvim/.config/nvim/lua/plugins/vim_mux.lua
@@ -1,43 +1,43 @@
 return {
-	-- {
-	--     "christoomey/vim-tmux-navigator",
-	--     cmd = {
-	--         "TmuxNavigateLeft",
-	--         "TmuxNavigateDown",
-	--         "TmuxNavigateUp",
-	--         "TmuxNavigateRight",
-	--         "TmuxNavigatePrevious",
-	--     },
-	--     keys = {
-	--         { "<C-Left>",  "<cmd>TmuxNavigateLeft<cr>" },
-	--         { "<C-Down>",  "<cmd>TmuxNavigateDown<cr>" },
-	--         { "<C-Up>",  "<cmd>TmuxNavigateUp<cr>" },
-	--         { "<C-Right>",  "<cmd>TmuxNavigateRight<cr>" },
-	--         { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
-	--     },
-	--     config = function()
-	--         vim.g.tmux_navigator_save_on_switch = 1
-	--     end
-	-- },
-	{
-		"mrjones2014/smart-splits.nvim",
-		config = function()
-			require("smart-splits").setup()
-			vim.keymap.set("n", "<C-Left>", require("smart-splits").move_cursor_left)
-			vim.keymap.set("n", "<C-Down>", require("smart-splits").move_cursor_down)
-			vim.keymap.set("n", "<C-Up>", require("smart-splits").move_cursor_up)
-			vim.keymap.set("n", "<C-Right>", require("smart-splits").move_cursor_right)
-			vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
+    -- {
+    --     "christoomey/vim-tmux-navigator",
+    --     cmd = {
+    --         "TmuxNavigateLeft",
+    --         "TmuxNavigateDown",
+    --         "TmuxNavigateUp",
+    --         "TmuxNavigateRight",
+    --         "TmuxNavigatePrevious",
+    --     },
+    --     keys = {
+    --         { "<C-Left>",  "<cmd>TmuxNavigateLeft<cr>" },
+    --         { "<C-Down>",  "<cmd>TmuxNavigateDown<cr>" },
+    --         { "<C-Up>",  "<cmd>TmuxNavigateUp<cr>" },
+    --         { "<C-Right>",  "<cmd>TmuxNavigateRight<cr>" },
+    --         { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+    --     },
+    --     config = function()
+    --         vim.g.tmux_navigator_save_on_switch = 1
+    --     end
+    -- },
+    {
+        "mrjones2014/smart-splits.nvim",
+        config = function()
+            require("smart-splits").setup()
+            vim.keymap.set("n", "<C-Left>", require("smart-splits").move_cursor_left)
+            vim.keymap.set("n", "<C-Down>", require("smart-splits").move_cursor_down)
+            vim.keymap.set("n", "<C-Up>", require("smart-splits").move_cursor_up)
+            vim.keymap.set("n", "<C-Right>", require("smart-splits").move_cursor_right)
+            vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
 
             vim.keymap.set('n', '<A-Left>', require('smart-splits').resize_left)
             vim.keymap.set('n', '<A-Down>', require('smart-splits').resize_down)
             vim.keymap.set('n', '<A-Up>', require('smart-splits').resize_up)
             vim.keymap.set('n', '<A-Right>', require('smart-splits').resize_right)
 
-            vim.keymap.set('n', '<leader><C-Left>', require('smart-splits').swap_buf_left)
-            vim.keymap.set('n', '<leader><C-Down>', require('smart-splits').swap_buf_down)
-            vim.keymap.set('n', '<leader><C-Up>', require('smart-splits').swap_buf_up)
-            vim.keymap.set('n', '<leader><C-Right>', require('smart-splits').swap_buf_right)
-		end,
-	},
+            -- vim.keymap.set('n', '<leader><C-Left>', require('smart-splits').swap_buf_left)
+            -- vim.keymap.set('n', '<leader><C-Down>', require('smart-splits').swap_buf_down)
+            -- vim.keymap.set('n', '<leader><C-Up>', require('smart-splits').swap_buf_up)
+            -- vim.keymap.set('n', '<leader><C-Right>', require('smart-splits').swap_buf_right)
+        end,
+    },
 }
