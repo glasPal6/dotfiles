b6de90f99680cd26314d492c259220a18f77981d
glaspal6 <dylan.r.kamstra@gmail.com>
2025-03-13 12:28:31 +0200

    feat(nvim): Better ai

--- nvim/.config/nvim/lua/plugins/ai_assistants.lua
+++ nvim/.config/nvim/lua/plugins/ai_assistants.lua
@@ -1,103 +1,104 @@
 return {
 
-	-- For more advanced options
-	-- {
-	--     "github/copilot.vim",
-	-- },
+    -- For more advanced options
+    -- {
+    --     "github/copilot.vim",
+    -- },
 
-	{
-		"olimorris/codecompanion.nvim",
-		event = { "VeryLazy" },
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			{
-				"MeanderingProgrammer/render-markdown.nvim",
-				ft = { "markdown", "codecompanion" },
-				opts = {
-					file_types = { "markdown", "codecompanion" },
-					render_modes = true, -- Render in ALL modes
-					sign = {
-						enabled = false, -- Turn off in the status column
-					},
-				},
-			},
-		},
-		config = function()
-			require("codecompanion").setup({
-				adapters = {
-					copilot = function()
-						return require("codecompanion.adapters").extend("copilot", {
-							schema = {
-								model = {
-									-- default = "o1",
-									default = "claude-3.7-sonnet-thought",
-									-- default = "claude-3.7-sonnet",
-								},
-							},
-						})
-					end,
-				},
-				strategies = {
-					chat = { adapter = "copilot" },
-					inline = { adapter = "copilot" },
-					agent = { adapter = "copilot" },
-				},
-			})
+    {
+        "olimorris/codecompanion.nvim",
+        event = { "VeryLazy" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            {
+                "MeanderingProgrammer/render-markdown.nvim",
+                ft = { "markdown", "codecompanion" },
+                opts = {
+                    file_types = { "markdown", "codecompanion" },
+                    render_modes = true, -- Render in ALL modes
+                    sign = {
+                        enabled = false, -- Turn off in the status column
+                    },
+                },
+            },
+        },
+        config = function()
+            require("codecompanion").setup({
+                adapters = {
+                    copilot = function()
+                        return require("codecompanion.adapters").extend("copilot", {
+                            schema = {
+                                model = {
+                                    -- default = "o1",
+                                    -- default = "claude-3.7-sonnet-thought",
+                                    -- default = "claude-3.7-sonnet",
+                                    default = "claude-3.5-sonnet",
+                                },
+                            },
+                        })
+                    end,
+                },
+                strategies = {
+                    chat = { adapter = "copilot" },
+                    inline = { adapter = "copilot" },
+                    agent = { adapter = "copilot" },
+                },
+            })
 
-			vim.keymap.set({ "n", "v" }, "<C-f>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-			vim.keymap.set(
-				{ "n", "v" },
-				"<Leader>aa",
-				"<cmd>CodeCompanionChat Toggle<cr>",
-				{ noremap = true, silent = true }
-			)
-			vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+            vim.keymap.set({ "n", "v" }, "<C-f>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+            vim.keymap.set(
+                { "n", "v" },
+                "<Leader>aa",
+                "<cmd>CodeCompanionChat Toggle<cr>",
+                { noremap = true, silent = true }
+            )
+            vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
-			vim.cmd([[cab cc CodeCompanion]])
-		end,
-	},
+            vim.cmd([[cab cc CodeCompanion]])
+        end,
+    },
 
-	-- {
-	-- 	"yetone/avante.nvim",
-	-- 	event = "VeryLazy",
-	-- 	dependencies = {
-	-- 		"nvim-treesitter/nvim-treesitter",
-	-- 		"stevearc/dressing.nvim",
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"MunifTanjim/nui.nvim",
-	-- 		--- The below dependencies are optional,
-	-- 		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-	-- 		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-	-- 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-	-- 		"zbirenbaum/copilot.lua", -- for providers='copilot'
-	-- 		{
-	-- 			-- support for image pasting
-	-- 			"HakonHarnes/img-clip.nvim",
-	-- 			event = "VeryLazy",
-	-- 			opts = {
-	-- 				-- recommended settings
-	-- 				default = {
-	-- 					embed_image_as_base64 = false,
-	-- 					prompt_for_file_name = false,
-	-- 					drag_and_drop = {
-	-- 						insert_mode = true,
-	-- 					},
-	-- 					-- required for Windows users
-	-- 					use_absolute_path = true,
-	-- 				},
-	-- 			},
-	-- 		},
-	-- 		{
-	-- 			-- Make sure to set this up properly if you have lazy=true
-	-- 			"MeanderingProgrammer/render-markdown.nvim",
-	-- 			opts = {
-	-- 				file_types = { "markdown", "Avante" },
-	-- 			},
-	-- 			ft = { "markdown", "Avante" },
-	-- 		},
-	-- 	},
-	-- 	build = "make",
-	-- 	opts = { provider = "copilot", hints = { enabled = false } },
-	-- },
+    -- {
+    -- 	"yetone/avante.nvim",
+    -- 	event = "VeryLazy",
+    -- 	dependencies = {
+    -- 		"nvim-treesitter/nvim-treesitter",
+    -- 		"stevearc/dressing.nvim",
+    -- 		"nvim-lua/plenary.nvim",
+    -- 		"MunifTanjim/nui.nvim",
+    -- 		--- The below dependencies are optional,
+    -- 		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    -- 		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    -- 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    -- 		"zbirenbaum/copilot.lua", -- for providers='copilot'
+    -- 		{
+    -- 			-- support for image pasting
+    -- 			"HakonHarnes/img-clip.nvim",
+    -- 			event = "VeryLazy",
+    -- 			opts = {
+    -- 				-- recommended settings
+    -- 				default = {
+    -- 					embed_image_as_base64 = false,
+    -- 					prompt_for_file_name = false,
+    -- 					drag_and_drop = {
+    -- 						insert_mode = true,
+    -- 					},
+    -- 					-- required for Windows users
+    -- 					use_absolute_path = true,
+    -- 				},
+    -- 			},
+    -- 		},
+    -- 		{
+    -- 			-- Make sure to set this up properly if you have lazy=true
+    -- 			"MeanderingProgrammer/render-markdown.nvim",
+    -- 			opts = {
+    -- 				file_types = { "markdown", "Avante" },
+    -- 			},
+    -- 			ft = { "markdown", "Avante" },
+    -- 		},
+    -- 	},
+    -- 	build = "make",
+    -- 	opts = { provider = "copilot", hints = { enabled = false } },
+    -- },
 }
